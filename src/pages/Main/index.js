1a82a8d6d34be42f64ac4c386177c67934459e64
marcelovinhas <marcelovinhascosta@hotmail.com>
2020-06-30 16:16:34 -0300

    7,8 - Adicionar repositório

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,30 +1,87 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 
-import { Container, Form, SubmitButton } from './styles';
+import api from '../../services/api';
 
-/*
-COMENTÁRIOS DE DENTRO DO RETURN
-quando quiser estilizar um elemento, o elemento precisa virar um componente
-criou <SubmitButton> em vez de apenas <button> para estilizar o botão
-*/
+import { Container, Form, SubmitButton, List } from './styles';
 
-export default function Main() {
-  return (
-    <Container>
-      <h1>
-        <FaGithubAlt />
-        Repositórios
-      </h1>
+export default class Main extends Component {
+  // passei para classe para ter state
+  // eslint-disable-next-line react/state-in-constructor
+  state = {
+    repositories: [],
+    newRepo: '',
+    loading: false, // para desabilitar o botão enquanto o repositório está sendo adicionado
+  };
 
-      <Form onSubmit={() => {}}>
-        <input type="text" placeholder="Adicionar repositório" />
+  handleInputChange = (e) => {
+    // handleInputChange recebe um evento
+    this.setState({ newRepo: e.target.value });
+  };
 
-        <SubmitButton>
-          <FaPlus color="#FFF" size={14} />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
+  handleSubmit = async (e) => {
+    e.preventDefault(); // para não atualizar a página quando adicionar um repositório
+
+    this.setState({ loading: true }); // quando clica no botão, desabilita loading
+
+    const { newRepo, repositories } = this.state; // pega newRepo e repositories de state
+
+    const response = await api.get(`/repos/${newRepo}`);
+
+    const data = {
+      // pegar informações da api
+      name: response.data.full_name,
+    };
+
+    this.setState({
+      repositories: [...repositories, data],
+      // ...repositories copia tudo que ja tem em repositories e data adiciona a nova informação
+      newRepo: '',
+      loading: false, // quando termina de adicionar, o botão habilita de novo
+    });
+  };
+
+  render() {
+    const { newRepo, repositories, loading } = this.state; // pega newRepo, repositories e loading de state
+
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositórios
+        </h1>
+
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Adicionar repositório"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+
+          <SubmitButton loading={loading}>
+            {/* quando quiser estilizar um elemento, o elemento precisa virar um componente */}
+            {/* criou <SubmitButton> em vez de apenas <button> para estilizar o botão */}
+            {loading ? ( // se loading for true coloca FaSpinner, se não FaPlus
+              <FaSpinner color="#FFF" size={14} />
+            ) : (
+              <FaPlus color="#FFF" size={14} />
+            )}
+          </SubmitButton>
+        </Form>
+
+        <List>
+          {repositories.map((repository) => (
+            // map precisa de key com uma informação única, a única informação única é o nome do repositório
+            <li key={repository.name}>
+              <span>{repository.name}</span>
+              {/* para mostrar o nome do repositório */}
+              <a href="">Detalhes</a>
+            </li>
+          ))}
+        </List>
+      </Container>
+    );
+  }
 }
