caa60d6120665e2a4115fbc04ae03c7e637dedc0
marcelovinhas <marcelovinhascosta@hotmail.com>
2020-06-30 12:44:01 -0300

    3 - Rotas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+/*
+ORGANIZAÇÃO DE PASTAS
+PAGES - páginas da aplicação
+MAIN - página principal
+REPOSITORY
+*/
+
 import React from 'react';
 
+import Routes from './routes';
+
 function App() {
-  return (
-    <div className="App">
-      <h1>SALVE SALVE</h1>{' '}
-    </div>
-  );
+  return <Routes />;
 }
 
 export default App;
---
+++ src/routes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+// BrowserRouter para usar localhost:3000/blablabla
+// Switch evita que mais de uma página seja chamada ao mesmo tempo
+// Route cada route representa uma página da aplicação
+
+import Main from './pages/Main';
+import Repository from './pages/Repository';
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Main} />
+        <Route path="/repository" component={Repository} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
