5e70b61b753c52c67013e72b5b1732d328c8e508
marcelovinhas <marcelovinhascosta@hotmail.com>
2020-06-30 13:56:27 -0300

    4, 5 Styled components e estilos globais

---
+++ src/pages/Main/styles.js
@@ -0,0 +1,10 @@
+/*
+consegue estilizar uma tag dentro do tittle
+consegue acessar propriedades do componente
+*/
+
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  color: #fff;
+`;
